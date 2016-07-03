b48477421d18112f2a0d2ddae005da2957249692
wangwentao <wangwentao@wangwentaodemacbook-pro.local>
2016-07-03 22:05:04 +0800

    Initial Commit

---
+++ git-wwt/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  git-wwt
+//
+//  Created by wangwentao on 16/7/3.
+//  Copyright © 2016年 wangwentao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
